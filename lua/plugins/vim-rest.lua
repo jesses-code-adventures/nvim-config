a2b41f9129b9a5f36ae395ed450c1df578e31778
jesse <jesse@rapidpaylegal.com>
2024-08-28 18:05:04 +1000

    feat: add vim-rest, update harpoon to accomodate new tmux config

---
+++ lua/plugins/vim-rest.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "diepm/vim-rest-console",
+    filetypes = "rest",
+    config = function()
+      vim.g.vrc_set_default_mapping = 0
+      vim.g.vrc_response_default_content_type = "application/json"
+      vim.g.vrc_output_buffer_name = "_OUTPUT.json"
+      vim.g.vrc_auto_format_response_patterns = {
+        json = "jq"
+      }
+      vim.api.nvim_set_keymap("n", "<leader>r", ":call VrcQuery()<CR>", {})
+    end,
+  }
+}
