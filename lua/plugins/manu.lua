89482c5bab5701e1e8977140b9a49048b8db6289
jesse <jesse@rapidpaylegal.com>
2024-08-31 18:46:28 +1000

    update: add manu

---
+++ lua/plugins/manu.lua
@@ -0,0 +1,22 @@
+local path = vim.fn.expand("~/coding/personal/manu.nvim")
+
+if not vim.loop.fs_stat(path) then
+    return {}
+end
+
+vim.opt.runtimepath:append(path)
+
+return {
+    {
+        "manu",
+        dir = path,
+        dev=true,
+        config = function()
+            local manu = require("manu")
+            manu.setup()
+            vim.keymap.set("n", "<leader>mo", function() manu.open_chat() end, {})
+            vim.keymap.set("n", "<leader>mr", function() manu.replace_and_prompt() end, {})
+        end,
+        lazy=false
+    }
+}
