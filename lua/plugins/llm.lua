9dc8bb6cc40b6162f01da2137bea6ef8752ce1f2
jesse <jesse@rapidpaylegal.com>
2025-02-26 13:11:53 +1100

    add sql instruction

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -22,9 +22,9 @@ return {
             excluded_providers = { }, -- options: openai, deepseek, google, anthropic, groq
             picker = 'telescope', -- pass nil, if you prefer a non-modifiable buffer to select your models from
             replace_prompt =
-            'You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks. Include a newline ("\n") at the beginning of any answer..',
+            'You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. If generating sql, always use lowercase where possible. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks.',
             help_prompt =
-            'You are a helpful assistant. What I have sent are my notes so far. You are very curt, yet helpful.'
+            'You are a helpful assistant. What I have sent are my notes so far. You are very curt, yet helpful. If generating sql, always use lowercase where possible.'
         },
         keys = {
             { '<leader>lr', function() require('llm').replace() end, mode = "n", { desc = 'llm replace codeblock' } },
