f84c77b390c7f1c370acd65e5f66e591d7ad06df
Jesse Williams <jesse@negativespacesounds.com>
2025-06-16 22:15:26 +1000

    improve dap experience/layout and lua ls lazy resolution

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -11,6 +11,19 @@ return {
             runtime = { version = "Lua 5.1" },
             diagnostics = {
                 globals = { "bit", "vim", "it", "describe", "before_each", "after_each", "os", "require" },
+            },
+            library = {
+                vim.fn.expand("$VIMRUNTIME/lua"),
+                vim.fn.expand("lua/lsp.lua"),
+                vim.fn.expand("$XDG_DATA_HOME/nvim/lazy/blink.cmp/lua"),
+                vim.fn.expand("$XDG_DATA_HOME/nvim/lazy/diffview.nvim/lua"),
+                vim.fn.expand("$XDG_DATA_HOME/nvim/lazy/fzf-lua/lua"),
+                vim.fn.expand("$XDG_DATA_HOME/nvim/lazy/lazy.nvim/lua"),
+                vim.fn.expand("$XDG_DATA_HOME/nvim/lazy/nvim-dap-go/lua"),
+                vim.fn.expand("$XDG_DATA_HOME/nvim/lazy/nvim-dap-ui/lua"),
+                vim.fn.expand("$XDG_DATA_HOME/nvim/lazy/nvim-dap/lua"),
+                vim.fn.expand("$XDG_DATA_HOME/nvim/lazy/nvim-treesitter/lua"),
+                vim.fn.expand("$XDG_DATA_HOME/nvim/lazy/plenary.nvim/lua"),
             }
         }
     },
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,24 +3,32 @@ return {
   dependencies = {
     'rcarriga/nvim-dap-ui',
     'nvim-neotest/nvim-nio', -- Required dependency for nvim-dap-ui
-    'leoluz/nvim-dap-go',
+    {
+      'leoluz/nvim-dap-go',
+      lazy = true,
+    },
+    {
+      'mfussenegger/nvim-dap-python',
+      lazy = true,
+    },
   },
   keys = {
-        { "<leader>b",  function() require("dap").toggle_breakpoint() end, { desc = "nvim-dap toggle breakpoint" } },
-        { "<leader>do", function() require("dapui").toggle() end,            { desc = "nvim-dap toggle" } },
-        { "<F1>",      function() require("dap").continue() end,     { desc = "nvim-dap continue" } },
-        { "<F2>",      function() require("dap").step_into() end,    { desc = "nvim-dap step into" } },
-        { "<F3>",      function() require("dap").step_over() end,    { desc = "nvim-dap step over" } },
-        { "<F4>",      function() require("dap").step_out() end,     { desc = "nvim-dap step out" } },
-        { "<F5>",      function() require("dap").step_back() end,    { desc = "nvim-dap step back" } },
-        { "<F12>",     function() require("dap").restart() end,      { desc = "nvim-dap restart" } },
-        { "<leader>?", function() require("dapui").eval() end, { desc = "nvim-dap eval" } },
+    { "<leader>b",  function() require("dap").toggle_breakpoint() end, { desc = "nvim-dap toggle breakpoint" } },
+    { "<leader>do", function() require("dapui").toggle() end,          { desc = "nvim-dap toggle" } },
+    { "<F1>",       function() require("dap").continue() end,          { desc = "nvim-dap continue" } },
+    { "<F2>",       function() require("dap").step_into() end,         { desc = "nvim-dap step into" } },
+    { "<F3>",       function() require("dap").step_over() end,         { desc = "nvim-dap step over" } },
+    { "<F4>",       function() require("dap").step_out() end,          { desc = "nvim-dap step out" } },
+    { "<F5>",       function() require("dap").step_back() end,         { desc = "nvim-dap step back" } },
+    { "<F11>",      function() require("dap").terminate() end,           { desc = "nvim-dap restart" } },
+    { "<F12>",      function() require("dap").restart() end,           { desc = "nvim-dap restart" } },
+    { "<leader>?",  function() require("dapui").eval() end,            { desc = "nvim-dap eval" } },
   },
   config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+    local dap = require('dap')
+    local dapui = require('dapui')
 
-    dapui.setup {
+    dapui.setup({
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
@@ -35,26 +43,30 @@ return {
           disconnect = '⏏',
         },
       },
-    }
+      layouts = {
+        {
+          elements = { "console", "repl" },
+          size = 10,
+          position = "bottom",
+        },
+        {
+          elements = {
+            { id = "scopes", size = 1 },
+            -- { id = "breakpoints", size = 0.5 },
+          },
+          size = 40,
+          position = "left",
+        },
+      },
+    })
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-    if vim.bo.filetype == 'go' then
-      dap.configurations.go = {
-        {
-          type = 'go',
-          name = 'Debug',
-          request = 'launch',
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-        },
-      }
-    end
     require('dap-go').setup {
       delve = {},
     }
+    require("dap-python").setup("uv")
   end,
 }
